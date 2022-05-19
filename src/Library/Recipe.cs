7fe943ddb90ad2be9ffb76ee4580d6ad30547373
BenjaminRielli <101676520+BenjaminRielli@users.noreply.github.com>
2022-05-19 19:25:09 -0300

    Modifique la clase Recipe y Program

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -15,9 +15,15 @@ namespace Full_GRASP_And_SOLID
 
         public Product FinalProduct { get; set; }
 
-        public void AddStep(Step step)
+        //public void AddStep(Step step)
+        //{
+        //    this.steps.Add(step);
+        //}
+
+        public void AddStep(Product input, double quantity, Equipment equipment, int time)
         {
-            this.steps.Add(step);
+            Step item = new Step(input, quantity, equipment, time);
+            this.steps.Add(item);
         }
 
         public void RemoveStep(Step step)
